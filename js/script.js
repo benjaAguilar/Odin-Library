4e8d4799ffb55ed9ce086e4396b96e48d2219796
benjaAguilar <benjaaguilar02@gmail.com>
2024-01-31 16:24:46 -0300

    added a description to the book object
    now you can put a book description

--- js/script.js
+++ js/script.js
@@ -3,17 +3,18 @@ const myLibrary = [];
 let addBook = document.querySelector("#add-book");
 let addButton = document.querySelector(".add-button");
 
-function Book(name, author, pages, read){
+function Book(name, description, author, pages, read){
 
     this.name = name;
+    this.description = description;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-function addBookToLibrary(name, author, pages, read){
+function addBookToLibrary(name, description, author, pages, read){
 
-    const book = new Book(name, author, pages, read)
+    const book = new Book(name, description, author, pages, read)
     myLibrary.push(book);
     console.log(myLibrary);
 }
@@ -22,4 +23,4 @@ addButton.addEventListener("click", () => {
     addBook.showModal();
 });
 
-addBookToLibrary("teton", "jorge espinoza", 188, false);
+addBookToLibrary("teton", "ashdjakshdjkashjdhahsdhkassh", "jorge espinoza", 188, false);
