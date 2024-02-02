38d6ae02574bd78d52ef2645b3966fb9d244ea76
benjaAguilar <benjaaguilar02@gmail.com>
2024-02-02 15:32:11 -0300

    split the function to show the book info
    that makes easy to add the placeholder book

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const myLibrary = [];
 let bookForm = document.querySelector("#book-form");
 let dialogAdd = document.querySelector(".dialog-add");
 let addBtn = document.querySelector("#add-btn");
+let exampleBook = document.querySelector(".example");
 
 let bookInfo = document.querySelector("#book-info");
 
@@ -23,38 +24,42 @@ function addBookToLibrary(name, description, author, pages, read){
     console.log(myLibrary.indexOf(book));
 }
 
+function showBookInfo(book){
+    let index;
+    let title = book.querySelector("p").textContent;
+
+    let description = document.querySelector(".description");
+    let author = document.querySelector(".author");
+    let pages = document.querySelector(".pages");
+    let read = document.querySelector("#read-info");
+
+    document.querySelector("#info-title").textContent = title;
+    document.querySelector(".title").textContent = title; 
+    
+    myLibrary.forEach((book) => {
+        if(book.name === title){
+            index = myLibrary.indexOf(book);
+            
+        }
+    });
+
+    description.textContent = myLibrary[index].description;
+    author.textContent = "By: " + myLibrary[index].author;
+    pages.textContent = "Pages: " + myLibrary[index].pages;
+    read.checked = myLibrary[index].read;
+    
+    read.addEventListener("change", () => {
+        myLibrary[index].read = read.checked;
+
+    });
+
+    bookInfo.showModal();
+}
+
 //Add event listener for every book created
 function createEvent(book){
     book.addEventListener("click", () => {
-        let index;
-        let title = book.querySelector("p").textContent;
-
-        let description = document.querySelector(".description");
-        let author = document.querySelector(".author");
-        let pages = document.querySelector(".pages");
-        let read = document.querySelector("#read-info");
-
-        document.querySelector("#info-title").textContent = title;
-        document.querySelector(".title").textContent = title; 
-        
-        myLibrary.forEach((book) => {
-            if(book.name === title){
-                index = myLibrary.indexOf(book);
-                
-            }
-        });
-
-        description.textContent = myLibrary[index].description;
-        author.textContent = "By: " + myLibrary[index].author;
-        pages.textContent = "Pages: " + myLibrary[index].pages;
-        read.checked = myLibrary[index].read;
-        
-        read.addEventListener("change", () => {
-            myLibrary[index].read = read.checked;
-
-        });
-
-        bookInfo.showModal();
+        showBookInfo(book);
     });
 }
 
@@ -98,4 +103,16 @@ addBtn.addEventListener("click", () => {
         printBook(name);
 
     }
+});
+
+let example = new Book(
+    "The Alchemyst",
+    "A special 25th anniversary edition of the extraordinary international bestseller, including a new Foreword by Paulo Coelho Combining magic, mysticism, wisdom and wonder into an inspiring tale of self-discovery, The Alchemist has become a modern classic, selling millions of copies around the world and transforming the lives of countless readers across generations. Paulo Coelhos masterpiece tells the mystical story of Santiago, an Andalusian shepherd boy who yearns to travel in search of a worldly treasure. His quest will lead him to riches far different—and far more satisfying—than he ever imagined. Santiagos journey teaches us about the essential wisdom of listening to our hearts, of recognizing opportunity and learning to read the omens strewn along lifes path, and, most importantly, to follow our dreams.",
+    "Paulo Coelho",
+    208,
+    true);
+
+myLibrary.push(example);
+exampleBook.addEventListener("click", () => {
+    showBookInfo(exampleBook);
 });
